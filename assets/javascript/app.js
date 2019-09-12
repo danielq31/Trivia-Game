f8dbdd198fc0b1e98f692045c6f88f4a1833220f
daniel quintero <danielq1143@gmail.com>
2019-09-11 22:06:07 -0700

    added timer functions,user functions, reset button,and start button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,43 @@
-let counter = 30;
+let counter = 10;
 let currentQuestion = 0;
 let score =0; 
 let lost =0;
 let timer;
+        // if timer is over go to next question
+    function nextQuestion() {
+        const isQuestionOver = (triviaQuestions.length - 1) === currentQuestion;
+        if (isQuestionOver) {
+            displayResult();
+        }else{
+            currentQuestion++;
+            loadQuestion();
+        }
+
+    }
+    
+function timeUp() {
+    clearInterval(timer);
+    
+        lost++;
+
+        nextQuestion();
+}
+
+    function countDown() {
+        counter--;
+
+        $('#time').html('timer: ' + counter);
+        if (counter ===0) {
+            timeUp();
+           
+        }
+    }
 
 function loadQuestion() {
+    counter = 10;
+    timer = setInterval(countDown, 1000);
+
+
     const question = triviaQuestions[currentQuestion].question; //
     const choices = triviaQuestions[currentQuestion].choices; //
 
@@ -24,5 +57,44 @@ function loadChoices(choices) {
         return result;
 
 }
+    // when right/wrong choice is picked go to next question
+$(document).on('click', '.choice', function() {
+   clearInterval(timer);
+     const selectedAnswer = $(this).attr('data-answer');
+    const correctAnswer = triviaQuestions[currentQuestion].correctAnswer;
+    
+    if (correctAnswer === selectedAnswer) {
+        score++;
+        nextQuestion();
+    }else{
+        lost++;
+        nextQuestion();
+    }
+});
 
-loadQuestion();
+function displayResult() {
+    const result =`
+    <p>your Score ${score}</p>
+    <p>you Missed ${lost}</p>
+    <p>Total Questions ${triviaQuestions.length}</p>
+    <button class="btn btn-primary" id="reset">Reset Game</button>
+    `;
+    
+    $('#game').html(result);
+}
+
+$(document).on('click', '#reset', function(){
+    counter = 10;
+    currentQuestion = 0;
+    score =0; 
+    lost =0;
+    timer = null;
+    loadQuestion();
+});
+
+
+$('#start').click(function() {
+    $('#start').remove();
+    $('#timer').html(counter);
+    loadQuestion();
+});
--- assets/javascript/questions.js
+++ assets/javascript/questions.js
@@ -1,25 +1,25 @@
 const triviaQuestions = [
     {
-    question: "what is my full name?",
-    choices: ["john doe", "dan johnson", "daniel quintero","ted"],
+    question: "what color is the sky?",
+    choices: ["orange", "blue", "purple","red"],
     correctAnswer: "daniel quintero"
     },
     
         {
-        question: "what is my full name?",
-        choices: ["john doe", "dan johnson", "daniel quintero","ted"],
+        question: "what animal is not a reptile?",
+        choices: ["snake", "turtle", "lizard","rabbit"],
         correctAnswer: "daniel quintero"
         },
        
             {
-            question: "what is my full name?",
-            choices: ["john doe", "dan johnson", "daniel quintero","ted"],
+            question: "how many days are in a week?",
+            choices: ["4", "9", "7","5"],
             correctAnswer: "daniel quintero"
             },
     
         {
-        question: "what is my full name?",
-        choices: ["john doe", "dan johnson", "daniel quintero","ted"],
+        question: "in a basketball game, how many players can be on the court for each team?",
+        choices: ["5", "10", "4","6"],
         correctAnswer: "daniel quintero"
         },
         {
